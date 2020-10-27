3deb0577015155dc51c0b6bab731839ccd89cf97
S-inTheory <livsi48@gmail.com>
2020-10-27 13:23:34 +0300

    added Book, Author, Publisher models

--- book_site/models.py
+++ book_site/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=100, unique=True)
+    cover = models.CharField(max_length=600)
+    pages = models.CharField(max_length=4)
+    chitai_gorod_price = models.CharField(max_length=4, null=True, default='нет в наличии')
+    book_24_price = models.CharField(max_length=4, null=True, default='нет в наличии')
+    labirint_price = models.CharField(max_length=4, null=True, default='нет в наличии')
+
+
+
