467a0a04627e0e0556819b24585047da03693968
S-inTheory <livsi48@gmail.com>
2020-10-29 14:01:27 +0300

    finished parser for labirint shop

--- book_site/parsers/book_find.py
+++ book_site/parsers/book_find.py
@@ -1,3 +1,5 @@
+import json
+from collections import OrderedDict
 from pprint import pprint
 
 import bs4
@@ -11,8 +13,16 @@ def labirint_search(book):
                     f'=&price_max=')
     soup = bs4.BeautifulSoup(html, 'html.parser')
     labirint_find = soup.find('div', class_='products-row')
-    titles = [title.text.strip('\n') for title in list(labirint_find.find_all('a', class_='product-title-link'))]
-    author = ''
-    return titles
+    result = OrderedDict({'title': [title.text.replace('\n', '') for title in
+                                    list(labirint_find.find_all('a', class_='product-title-link'))],
+                          'author': [author.text.replace('\n', '').rstrip() for author in
+                                     list(labirint_find.find_all('div', class_='product-author'))],
+                          'pubhouse': [pubhouse.text.replace('\n', '') for pubhouse in
+                                       list(labirint_find.find_all('div', class_='product-pubhouse'))],
+                          'covers': [image['data-src'] for image in labirint_find.find_all('img')],
+                          'labirint_price': [price.text.strip('₽\r\n\t\t').rstrip() for price in
+                                             list(labirint_find.find_all('span', class_='price-val'))],
+                          'labirint_link': [f"https://www.labirint.ru{link['href']}" for link in
+                                            labirint_find.find_all('a', class_='cover')]})
 
-print(labirint_search('томминокеры'))
+    return result
