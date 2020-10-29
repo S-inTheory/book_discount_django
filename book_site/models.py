0c1e18780bffed5c255ebf2ead92d3dbb2b9fa99
S-inTheory <livsi48@gmail.com>
2020-10-29 11:23:00 +0300

    - Rename field publisher on books to pubhouse - Rename field name on publisher to pubhouse - Add field full_name_chitai_gorod to authors

--- book_site/models.py
+++ book_site/models.py
@@ -3,16 +3,17 @@ from django.db import models
 
 class Authors(models.Model):
     full_name = models.CharField(max_length=100)
+    full_name_chitai_gorod = models.CharField(max_length=100, null=True)
 
 
 class Publisher(models.Model):
-    name = models.CharField(max_length=100)
+    pubhouse = models.CharField(max_length=100)
 
 
 class Books(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Authors, on_delete=models.CASCADE)
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubhouse = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     cover = models.CharField(max_length=600)
     labirint_price = models.CharField(max_length=4, null=True, default='нет в наличии')
     labirint_link = models.CharField(max_length=200, null=True, default='#')
