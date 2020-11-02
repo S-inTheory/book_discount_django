0d2addb7c9f95387f0871f8e97a73374edbc6187
S-inTheory <livsi48@gmail.com>
2020-11-02 14:06:48 +0300

    Renamed Authors to LabirintAuthors, Publisher to LabirintPublisher in foreign keys

--- book_site/models.py
+++ book_site/models.py
@@ -13,8 +13,8 @@ class LabirintPublisher(models.Model):
 class Labirint(models.Model):
     labirint_id = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
-    pubhouse = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    author = models.ForeignKey(LabirintAuthors, on_delete=models.CASCADE)
+    pubhouse = models.ForeignKey(LabirintPublisher, on_delete=models.CASCADE)
     cover = models.CharField(max_length=600)
     labirint_price = models.CharField(max_length=4, null=True, default='нет в наличии')
     labirint_link = models.CharField(max_length=200, null=True, default='#')
