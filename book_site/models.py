120f9a0f0ad12f939bd94d2b8d7bf68986a13f49
S-inTheory <livsi48@gmail.com>
2020-11-04 16:02:40 +0300

    corrected errors in names of ForeignKeys after renaming models

--- book_site/models.py
+++ book_site/models.py
@@ -12,7 +12,7 @@ class LabirintPublisher(models.Model):
 class Labirint(models.Model):
     labirint_id = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(LabirintAuthors, on_delete=models.CASCADE)
+    author = models.ForeignKey(LabirintAuthor, on_delete=models.CASCADE)
     pubhouse = models.ForeignKey(LabirintPublisher, on_delete=models.CASCADE)
     cover = models.CharField(max_length=600)
     labirint_price = models.CharField(max_length=4, null=True, default='нет в наличии')
@@ -26,7 +26,7 @@ class Book24Author(models.Model):
 class Book24(models.Model):
     book24_id = models.CharField(max_length=100, unique=True)
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Book24Authors, on_delete=models.CASCADE)
+    author = models.ForeignKey(Book24Author, on_delete=models.CASCADE)
     cover = models.CharField(max_length=100)
     book24_price = models.CharField(max_length=5)
     book24_link = models.CharField(max_length=200)
