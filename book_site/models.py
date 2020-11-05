7c571a624319d8b844ff0524ebbb8bf7ac4c59bb
S-inTheory <livsi48@gmail.com>
2020-11-05 15:26:34 +0300

    deleted models chitaigorodautho and chitaigorod

--- book_site/models.py
+++ book_site/models.py
@@ -31,16 +31,3 @@ class Book24(models.Model):
     book24_price = models.CharField(max_length=5)
     book24_link = models.CharField(max_length=200)
 
-
-class ChitaiGorodAuthor(models.Model):
-    full_name = models.CharField(max_length=100, unique=True)
-
-
-class ChitaiGorod(models.Model):
-    chitaigorod_id = models.CharField(max_length=100, unique=True)
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(ChitaiGorodAuthor, on_delete=models.CASCADE)
-    cover = models.CharField(max_length=100)
-    chitaigorod_price = models.CharField(max_length=5)
-    chitaigorod_link = models.CharField(max_length=200)
-
